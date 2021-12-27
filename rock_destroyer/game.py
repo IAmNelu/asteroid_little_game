24c308c72896228a976d6d7e26f6dcbb656d62db
Valentin Nelu Ifrim <ifrimvale@gmail.com>
2021-12-27 14:24:13 +0100

    add asteroids

--- rock_destroyer/game.py
+++ rock_destroyer/game.py
@@ -1,22 +1,29 @@
 import pygame
-from utils import load_sprite
+from utils import load_sprite, get_random_position
 from models import Spaceship, Asteroid
+
 import os
 
 clear = lambda: os.system('cls')
 
 
+MIN_ASTEROID_DISTANCE = 250
 class RockDestryer:
-
   def __init__(self):
     self._init_pygame()
     self.screen = pygame.display.set_mode((800, 600))
     self.background = load_sprite("space", type="jpg", with_alpha=False)
     self.clock = pygame.time.Clock()
     
-    self.asteroids = [Asteroid((0,0)) for _ in range(6)]
+    self.asteroids = []
     self.spaceship = Spaceship((400, 300))  
     
+    for _ in range(6):
+      while True:
+        position = get_random_position(self.screen)
+        if (position.distance_to(self.spaceship.position) > MIN_ASTEROID_DISTANCE):
+          break
+      self.asteroids.append(Asteroid(position))
 
   def main_loop(self):
     while True:
@@ -35,18 +42,24 @@ class RockDestryer:
         quit()
     
     is_key_pressed = pygame.key.get_pressed()
-    
-    if is_key_pressed[pygame.K_RIGHT]:
-      self.spaceship.rotate(clockwise=True)
-    elif is_key_pressed[pygame.K_LEFT]:
-      self.spaceship.rotate(clockwise=False)
-    if is_key_pressed[pygame.K_UP]:
-      self.spaceship.accelerate()
+    if self.spaceship:
+      if is_key_pressed[pygame.K_RIGHT]:
+        self.spaceship.rotate(clockwise=True)
+      elif is_key_pressed[pygame.K_LEFT]:
+        self.spaceship.rotate(clockwise=False)
+      if is_key_pressed[pygame.K_UP]:
+        self.spaceship.accelerate()
       
 
   def _process_game_logic(self):
     for game_object in self._get_game_objects():
       game_object.move(self.screen)
+    
+    if self.spaceship:
+      for asteroid in self.asteroids:
+        if asteroid.collides_with(self.spaceship):
+          self.spaceship = None
+          break
 
   def _draw(self):
     # self.screen.fill((0, 0, 255))
@@ -58,4 +71,7 @@ class RockDestryer:
     self.clock.tick(60)
 
   def _get_game_objects(self):
-    return [*self.asteroids, self.spaceship]
+    game_objs = [*self.asteroids]
+    if self.spaceship:
+      game_objs.append(self.spaceship)
+    return game_objs
